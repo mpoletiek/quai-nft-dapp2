c3eafc183503efaffec92445db18f6712041d49c
mpoletiek <matthew.poletiek@gmail.com>
2024-12-12 13:40:01 -0600

    walkthrough

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,16 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Providers } from "@/app/providers";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
+const geistSans = Geist({
   variable: "--font-geist-sans",
-  weight: "100 900",
+  subsets: ["latin"],
 });
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
+
+const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
-  weight: "100 900",
+  subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
@@ -19,21 +18,22 @@ export const metadata: Metadata = {
   description: "An example NFT dApp on Quai",
 };
 
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <Providers>
-          {children}
-        </Providers>
-        
-      </body>
-    </html>
+	<html lang="en">
+  	<body
+    	className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+  	>
+    	<Providers>
+      	{children}
+    	</Providers>
+   	 
+  	</body>
+	</html>
   );
 }
